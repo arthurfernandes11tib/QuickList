c9baac2dd1c5f3ce9764b0cbccbcd44159a2c8fd
arthurfernandes11tib fernandes <arthusfeernandes99@gmail.com>
2025-06-08 17:47:07 +0000

    adição da verificação do checked na input

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function showItemsList() {
         sectionList.innerHTML += `
         <div class="item">
                 <div>
-                    <input type="checkbox" name="list" id="item-${index}">
+                     <input type="checkbox" name="list" id="item-${index}" ${item.checked ? "checked" : ""}>
                     <div class="custom-checkbox" onClick="checkItem('${item.name}')">
                         <img src="./assets/checked.svg" alt="checked">
                     </div>
@@ -56,6 +56,6 @@ function removeItem(itemName) {
 
 function checkItem(itemName) {
     const itemIndex = items.findIndex((item) => item.name === itemName)
-    items.checked = !item.checked
+     items[itemIndex].checked = !items[itemIndex].checked
     showItemsList()
 }
